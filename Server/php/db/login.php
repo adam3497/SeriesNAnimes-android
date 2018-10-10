6aa268f392cee0235194d5dfb02551e78b710089
adam3497 <amador19972012@gmail.com>
2018-10-10 00:54:36 -0600

    adding the new login page

---
+++ Server/php/db/login.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adma9717
+ * Date: 09/10/18
+ * Time: 10:50 PM
+ */
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+echo 'Nombre de usuario: ' . $username . '\n' . 'Contraseña: ' . $password;
