f6855731a473c1a73394c8b3146f72d2842b7ae4
adam3497 <amador19972012@gmail.com>
2019-05-06 14:03:21 -0600

    Cambios en las credenciales de la base de datos y modificaciones al ignore

--- Server/php/db/DatabaseCredentials.php
+++ Server/php/db/DatabaseCredentials.php
@@ -1,12 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: adma9717
- * Date: 29/09/18
- * Time: 10:56 AM
- */
-
 $db_name = 'SeriesNAnimes';
 $db_user = 'adam3497';
-$db_password = 'onigiri9717';
+$db_password = 'oniniri9717';
 $db_host = 'localhost';
